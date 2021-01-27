8730060da8075648420c181c3a5d1b6658cb1502
AviralT <75325409+AviralT@users.noreply.github.com>
2021-01-28 00:03:56 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './Navbar'
-
+import Footer from './Footer';
 function App() {
   return (
     <div className="App">
+    <div className= "cwp">
       <Navbar />
+      </div>
+      <Footer />
     </div>
   );
 }
