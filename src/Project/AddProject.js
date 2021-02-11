ee87ff896ab1155262ab8ec52a4b02c594eb36b2
AviralT <75325409+AviralT@users.noreply.github.com>
2021-02-12 02:32:33 +0530

    project form added

--- src/Project/AddProject.js
+++ src/Project/AddProject.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Component } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
+import Navbar from "../Common/Navbar";
+import "./AddProjects.css";
 
 class AddProject extends Component {
-
   state = {
     title: "",
     description: "",
@@ -50,42 +51,112 @@ class AddProject extends Component {
     .then(response => response.json())
     .then(res => console.log(res));
   }
+  styling = {
+    border: "0",
+    borderBottom: "2px solid #c4b6b6",
+    borderRadius: "0",
+    width: "670px",
+  }
+
 
   render() {
     return (
-      <Form >
-        <Form.Group controlId="formBasicTitle">
-          <Form.Label>Title</Form.Label>
-          <Form.Control name="title" type="title" onChange={this.changeFieldsHandler("title")} />
-        </Form.Group>
-        <Form.Group controlId="formBasicDescription">
-          <Form.Label>Description</Form.Label>
-          <Form.Control name="description" type="description" onChange={this.changeFieldsHandler("description")} />
-        </Form.Group>
-        <Form.Group controlId="formBasicTechStack">
-          <Form.Label>Tech Stack</Form.Label>
-          <Form.Control name="techStack" type="techStack" onChange={this.changeFieldsHandler("techStack")} />
-        </Form.Group>
-        <Form.Group controlId="formBasicGithub">
-          <Form.Label>Github</Form.Label>
-          <Form.Control name="github" type="github" onChange={this.changeFieldsHandler("github")} />
-        </Form.Group>
-        <Form.Group controlId="formBasicLink">
-          <Form.Label>Hosted Link</Form.Label>
-          <Form.Control name="hosted" type="hosted" onChange={this.changeFieldsHandler("hosted")} />
-        </Form.Group>
-        <Form.Group controlId="formBasicDifficulty">
-          <Form.Label>Difficulty</Form.Label>
-          <Form.Control name="difficulty" type="difficulty" onChange={this.changeFieldsHandler("difficulty")} />
-        </Form.Group>
-        <Form.Group controlId="formBasicField">
-          <Form.Label>Field</Form.Label>
-          <Form.Control name="field" type="field" onChange={this.changeFieldsHandler("field")} />
-        </Form.Group>
-        <Button onClick={this.submitHandler} variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div style={{ backgroundColor: "#dfeae9", height: "100vh" }}>
+        <Navbar />
+        <Container className="container" style={{ height: "auto" }}>
+          <div className="addp">
+          <h2 className="loginHead">Add your Work</h2>
+            <Form >
+              <Form.Group controlId="formBasicTitle">
+                <Form.Control 
+                    style={{
+                      ...this.styling,
+                    }}
+                    name="title" 
+                    type="title"
+                    placeholder="Title"
+                    onChange={this.changeFieldsHandler("title")} />
+              </Form.Group>
+              <Form.Group controlId="formBasicDescription">
+                <Form.Control 
+                    as="textarea" rows={1}
+                    style={{
+                      ...this.styling,
+                      
+                    }}
+                    name="description" 
+                    type="description" 
+                    placeholder="Description"
+                    onChange={this.changeFieldsHandler("description")} />
+              </Form.Group>
+              <Form.Group controlId="formBasicTechStack">
+                <Form.Control style={{
+                      ...this.styling,
+                     
+                    }} 
+                    name="techStack" 
+                    type="techStack" 
+                    placeholder="Tech Stack"
+                    onChange={this.changeFieldsHandler("techStack")} />
+              </Form.Group>
+              <Form.Group controlId="formBasicGithub">
+                <Form.Control style={{
+                      ...this.styling,
+                     
+                    }} 
+                    name="github" 
+                    type="github" 
+                    placeholder="Github"
+                    onChange={this.changeFieldsHandler("github")} />
+              </Form.Group>
+              <Form.Group controlId="formBasicLink">
+                <Form.Control style={{
+                      ...this.styling,
+                     
+                    }}
+                    name="hosted" 
+                    type="hosted"
+                    placeholder="Hosted Link"
+                    onChange={this.changeFieldsHandler("hosted")} />
+              </Form.Group>
+              <Form.Group controlId="formBasicDifficulty">
+                <Form.Control style={{
+                      ...this.styling,
+                    }} 
+                    placeholder="Difficulty"
+                    as="select" size="sm">
+                    <option value="" disabled selected>Difficulty</option>
+                    <option>Beginner</option>
+                    <option>Intermediate</option>
+                    <option>Expert</option>
+                    name="difficulty"
+                    type="difficulty" 
+                    
+                    onChange={this.changeFieldsHandler("difficulty")}
+                </Form.Control>
+              </Form.Group>
+              <Form.Group controlId="formBasicField">
+                <Form.Control style={{
+                     ...this.styling,
+                    }} 
+                    as="select" size="sm">
+                    <option value="" disabled selected>Field</option>
+                    <option>Machine Learning</option>
+                    <option>Web Development</option>
+                    <option>Artificial Intelligence</option>
+                    name="field" 
+                    type="field" 
+                    placeholder="Field" 
+                    onChange={this.changeFieldsHandler("field")}
+                    </Form.Control>
+              </Form.Group>
+              <Button onClick={this.submitHandler} variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </Container>
+      </div>
     );
   }
 };
